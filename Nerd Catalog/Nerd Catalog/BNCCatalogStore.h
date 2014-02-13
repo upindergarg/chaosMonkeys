0f931b803939441dbc70696e74ae909b337c7b4e
Jimmy <Upinder.Garg@monitise.com>
2014-02-13 15:42:53 -0800

    Adding AFNetworking framework

---
+++ Nerd Catalog/Nerd Catalog/BNCCatalogStore.h
@@ -0,0 +1,13 @@
+//
+//  BNCCatalogStore.h
+//  Nerd Catalog
+//
+//  Created by Jimmy Garg on 2/13/14.
+//  Copyright (c) 2014 Jimmy Garg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNCCatalogStore : NSObject
+
+@end
---
+++ Nerd Catalog/Nerd Catalog/BNCCatalogStore.m
@@ -0,0 +1,13 @@
+//
+//  BNCCatalogStore.m
+//  Nerd Catalog
+//
+//  Created by Jimmy Garg on 2/13/14.
+//  Copyright (c) 2014 Jimmy Garg. All rights reserved.
+//
+
+#import "BNCCatalogStore.h"
+
+@implementation BNCCatalogStore
+
+@end
