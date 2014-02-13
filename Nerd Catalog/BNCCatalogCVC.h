5db2b9798bf91fccdc1f467229a4317acbd06d97
J. HOWARD SMART <hsmart@me.com>
2014-02-13 13:11:34 -0800

    Changed one vc to collection view controller

---
+++ Nerd Catalog/BNCCatalogCVC.h
@@ -0,0 +1,13 @@
+//
+//  BNCCatalogCVC.h
+//  Nerd Catalog
+//
+//  Created by J. HOWARD SMART on 2/13/14.
+//  Copyright (c) 2014 Jimmy Garg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNCCatalogCVC : UICollectionViewController
+
+@end
---
+++ Nerd Catalog/BNCCatalogCVC.m
@@ -0,0 +1,38 @@
+//
+//  BNCCatalogCVC.m
+//  Nerd Catalog
+//
+//  Created by J. HOWARD SMART on 2/13/14.
+//  Copyright (c) 2014 Jimmy Garg. All rights reserved.
+//
+
+#import "BNCCatalogCVC.h"
+
+@interface BNCCatalogCVC ()
+
+@end
+
+@implementation BNCCatalogCVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
